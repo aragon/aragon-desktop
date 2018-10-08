b26c50348d8485654ac7db0ffbed9138480980e3
Luis Cuende <me@luisivan.net>
2018-10-08 18:24:36 +0200

    First version of app pinning

--- src/lib/ipfs-caching.js
+++ src/lib/ipfs-caching.js
@@ -1,8 +1,18 @@
+const url = require('url')
 const { IpfsConnector } = require('@akashaproject/ipfs-connector')
+const { session } = require('electron')
 const Store = require('electron-store')
 const store = new Store()
 
+// 7 days is the default expiration time
+// if the pinned hash is not accessed
+const IPFS_EXPIRATION = 7
+
 const instance = IpfsConnector.getInstance()
+
+const ipfsFilter = {
+  urls: ['https://localhost:8080/ipfs/*']
+}
 
 function pinAragonCore (newHash) {
   const storedHash = store.get('aragon.aragonpm.eth')
@@ -13,4 +23,34 @@ function pinAragonCore (newHash) {
   }
 }
 
-module.exports = { pinAragonCore }
+function updateExpiration (hash) {
+  const expirationDate = new Date()
+  expirationDate.setDate((expirationDate.getDate() + IPFS_EXPIRATION))
+  store.set(`${hash}-expiration`, expirationDate.getTime())
+}
+
+function pinIpfsResources () {
+  session.defaultSession.webRequest.onBeforeSendHeaders(ipfsFilter, (details, cb) => {
+    const path = url.parse(details.url).path
+    if (path.startsWith('/ipfs/')) {
+      const hash = path.split('/')[2]
+      // If it has already been pinned, check its expiration and act accordingly
+      if (store.has(`${hash}-expiration`)) {
+        const hashExpiration = store.get(`${hash}-expiration`)
+        if (hashExpiration < new Date().getTime()) {
+          instance.api.apiClient.pin.rm(hash)
+          store.delete(`${hash}-expiration`)
+        } else {
+          updateExpiration(hash)
+        }
+      // If not, pin it and set a new expiration
+      } else {
+        updateExpiration(hash)
+        instance.api.apiClient.pin.add(hash)
+      }
+    }
+    cb({ cancel: false, requestHeaders: details.requestHeaders })
+  })
+} 
+
+module.exports = { pinAragonCore, pinIpfsResources }
