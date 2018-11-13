d5928575c3292f512198fef2993299c6df75c499
Luis Iván Cuende <me@luisivan.net>
2018-11-13 15:39:31 +0100

    Using eth-provider (#14)

--- src/lib/aragon-core.js
+++ src/lib/aragon-core.js
@@ -1,16 +1,14 @@
 const Web3 = require('web3')
 const APM = require('@aragon/apm')
-const { createWeb3Provider } = require('./web3-provider')
+const provider = require('eth-provider')
 
 async function getLatestFromRepo (repo) {
-  const provider = createWeb3Provider()
-
   // Local
   // const ensRegistryAddress = '0x5f6F7E8cc7346a11ca2dEf8f827b7a0b612c56a1'
   // Rinkeby
   const ensRegistryAddress = '0xfbae32d1cde62858bc45f51efc8cc4fa1415447e'
   const apm = APM(
-    new Web3(provider),
+    new Web3(provider(['injected', 'frame'])),
     { ensRegistryAddress, ipfs: { host: 'localhost', protocol: 'http', port: 5001 } }
   )
 
