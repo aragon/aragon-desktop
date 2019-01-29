b51f45c0ed478acd3ce5d35c5ba5a5ee8827328e
Brett Sun <qisheng.brett.sun@gmail.com>
2019-01-29 01:38:42 +0100

    feat: increase default width (#36)

--- src/main.js
+++ src/main.js
@@ -39,7 +39,7 @@ async function start (mainWindow) {
 
 function createWindow () {
   const mainWindowState = windowStateKeeper({
-    defaultWidth: 1000,
+    defaultWidth: 1200,
     defaultHeight: 800
   })
 
