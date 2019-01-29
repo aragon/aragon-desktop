7b58ae71062ca538a8d6db67167dd7beb9a2a74a
Brett Sun <qisheng.brett.sun@gmail.com>
2019-01-29 03:52:01 +0100

    fix: properly wait for all promises

--- src/lib/ipfs-caching.js
+++ src/lib/ipfs-caching.js
@@ -19,7 +19,7 @@ const ipfsFilter = {
 
 async function pinAragonClientForNetwork (newHash, network) {
   const storageKey = `${network}:aragon.aragonpm.eth`
-  const storedHash = storage.get(storageKey)
+  const storedHash = await storage.get(storageKey)
   if (storedHash !== newHash) {
     await ipfsPinAdd(newHash)
     if (storedHash) {
@@ -50,13 +50,13 @@ async function purgeUnusedIpfsResources () {
   console.log('Purged old IPFS resources')
 }
 
-function pinIpfsResources () {
-  session.defaultSession.webRequest.onBeforeSendHeaders(ipfsFilter, (details, cb) => {
+function listenAndPinResources () {
+  session.defaultSession.webRequest.onBeforeSendHeaders(ipfsFilter, async (details, cb) => {
     const path = url.parse(details.url).path
     if (path.startsWith('/ipfs/')) {
       const hash = path.split('/')[2]
-      if (!storage.has(hash)) {
-        ipfsPinAdd(hash)
+      if (!(await storage.has(hash))) {
+        await ipfsPinAdd(hash)
       }
       updateExpiration(hash)
       console.log('Pinned new IPFS resource:', hash)
@@ -65,4 +65,4 @@ function pinIpfsResources () {
   })
 }
 
-module.exports = { pinAragonClientForNetwork, pinIpfsResources, purgeUnusedIpfsResources }
+module.exports = { listenAndPinResources, pinAragonClientForNetwork, purgeUnusedIpfsResources }
